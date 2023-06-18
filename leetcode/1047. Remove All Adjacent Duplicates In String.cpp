8a822152b8125228a870a8405b33448f0f7f6e29
Nishtha Jain <127538617+Nishtha-Jain-1119@users.noreply.github.com>
2023-06-19 00:06:58 +0530

    Update 1047. Remove All Adjacent Duplicates In String.cpp

--- leetcode/1047. Remove All Adjacent Duplicates In String.cpp
+++ leetcode/1047. Remove All Adjacent Duplicates In String.cpp
@@ -2,7 +2,6 @@
 // You are given a string s consisting of lowercase English letters. A duplicate removal consists of choosing two adjacent and equal letters and removing them.
 
 // We repeatedly make duplicate removals on s until we no longer can.
-
 // Return the final string after all such duplicate removals have been made. It can be proven that the answer is unique.
 
 class Solution {
@@ -12,7 +11,8 @@ public:
         while(i<s.length()){
             if(s[i] == s[i+1]){
                 s.erase(i,2);
-                if(i>=1)i--;
+                if(i>=1) //edge case-> if the first element is duplicate.
+                    i--;
             }
             else{
                 i++;
