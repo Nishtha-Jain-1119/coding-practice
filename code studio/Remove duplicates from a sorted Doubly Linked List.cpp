c76125a8178801d646ceff6f6d3663e4305a3477
Nishtha Jain <127538617+Nishtha-Jain-1119@users.noreply.github.com>
2023-07-14 13:51:00 +0530

    Create Remove duplicates from a sorted Doubly Linked List.cpp

---
+++ code studio/Remove duplicates from a sorted Doubly Linked List.cpp
@@ -0,0 +1,52 @@
+// Remove duplicates from a sorted Doubly Linked List
+// Sample Input 1 :
+// 5
+// 1 2 2 2 3
+// Sample Output 1 :
+// 1 2 3
+
+/**
+ * Definition of doubly linked list:
+ * class Node {
+ * public:
+ *      int data;
+ *      Node *prev;
+ *      Node *next;
+ *      Node() {
+ *          this->data = 0;
+ *          this->prev = NULL;
+ *          this->next = NULL;
+ *      }
+ *      Node(int data) {
+ *          this->data = data;
+ *          this->prev = NULL;
+ *          this->next = NULL;
+ *      }
+ *      Node (int data, Node *next, Node *prev) {
+ *          this->data = data;
+ *          this->prev = prev;
+ *          this->next = next;
+ *      }
+ * };
+ *
+ *************************************************************************/
+
+Node * removeDuplicates(Node *head)
+{
+    // Write your code here
+
+    Node* curr = head;
+    while(curr != NULL){
+        if(curr->next != NULL && curr->data == curr->next->data){
+            Node* next_next = curr ->next -> next;
+            Node* nodeToDelete = curr -> next;
+            delete(nodeToDelete);
+            curr -> next = next_next;
+        }
+        else{
+            curr = curr->next;
+        }
+    }
+
+    return head;
+}
