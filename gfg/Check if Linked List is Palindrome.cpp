d3f50d8d11708713bbd4a1b4042748a7eafe4c7c
Nishtha Jain <127538617+Nishtha-Jain-1119@users.noreply.github.com>
2023-07-15 10:13:48 +0530

    Update Check if Linked List is Palindrome.cpp

--- gfg/Check if Linked List is Palindrome.cpp
+++ gfg/Check if Linked List is Palindrome.cpp
@@ -27,4 +27,63 @@ class Solution{
         return true;
     }
 };
-// ----------------------------------------------------------another approach-----------------------------------------------------------------
+// -------------------------------------------------another approach---with TC-O(n) and SC-O(1)----------------------------------------------
+class Solution{
+  private:
+    // function to find mid
+    Node* findMid(Node* head)
+    {
+        Node* slow = head;
+        Node* fast = head->next;
+        while(fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    // function to reverse a list
+    Node* reverseList(Node* head)
+    {
+        if(head->next == NULL){
+            return head;
+        }
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* forward = NULL;
+        while(curr != NULL){
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        if(head->next == NULL){
+            return true;
+        }
+        
+        // step-1 get mid node
+        Node* mid = findMid(head);
+        
+        // step-2 reverse the second half if linkedlist
+        Node* head2 = reverseList(mid->next);
+        mid->next = NULL;
+        
+        // now compare both lists
+        Node* head1 = head;
+        while(head1 != NULL && head2 != NULL)
+        {
+            if(head1->data != head2->data){
+                return false;
+            }
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+        return true;
+    }
+};
